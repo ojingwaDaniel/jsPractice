705ed8c865de9e90c171ef87bf0348c99bd657fb
Abigail <neneojingwa@gmail.com>
2022-07-25 03:36:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// PROGRAMME TO KNOW IF YOU ARE OLD ENOUGH TO VOTE
+let votersAge= 34;
+if (votersAge >=18) {
+    console.log('Congratulations you are qualified to vote, Make good use of your voting right');
+}
+else{
+    console.log('Ouch! Sorry you are not qualified to vote because you are less than 18');
+//        MY PROFILE
+let profile = {
+    'First-name': 'Daniel',
+    'Surname' : 'Ojingwa',
+    'Hobby' : ['Football','Music','Travelling','Driving','Coding'],
+    'Fav-Food': 'porridge yam'
+}
+//       FOR LOOP 
+let  barcaPlayers= ['Ter-stergen','Dest','Pique','Arajuo','Sergio Busquet','Gavi','Dembele','Pedri','lewandoski','Ansu Fati','Rapinha']
+for(let i = 0;i > barcaPlayers.length; i++){
+    console.log(i,barcaPlayers[i])
+}
+let barcaYoungStar = document.querySelector('.container')
+    //FUNCTIONS
+function  mutiply(digit1,digit2){
+    let answer = (digit1 * digit2)
+    return answer
+}
+let answer = mutiply(2,2)
