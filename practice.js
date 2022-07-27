57fd8e52f1816b2dbea884b4249b07eaf53a148e
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-07-27 05:07:37 +0100

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,31 @@
+'use strict'
+// DISRUTURING AN ARRAY 
+const youngStar = ['Dembele','pedri','Nico','fati','Arajuo','Gavi']
+const [Araj , Gavi] = youngStar
+let starboy = String( youngStar.slice(-3))
+console.log( `the most consistent bsrcelona players are ${starboy}`);
+let consistent = youngStar.slice(-3)
+consistent.forEach(player =>{
+ console.log(`The most consistent barcelona players is ${player}`)
+})
+console.log(consistent);
+// DISRUTURING AN OBJECT
+let veteran = {
+    'playersName':'Busquet',
+    'Potential Replacement': 'Nico',
+    'position': 'DMF',
+}
+let {playersName,position} = veteran
+console.log(`Barcelona most important verteran is ${playersName} and he is plays as a ${position}`)
+// DELETING AN OBJECT PROPERTIES
+let bigClubs = {
+    '1st': 'Real Madrid ',
+    '2nd': 'Barcelona ',
+    '3rd' : 'Bayern',
+    '4rd' : 'Liverpool',
+    '5th' : 'Man City'
+
+}
+delete bigClubs['5th']
+console.log(bigClubs)
+
