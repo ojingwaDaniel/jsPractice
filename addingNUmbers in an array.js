0850c8e66bafba291015f11ee5696343035b3880
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-07-28 16:39:57 +0100

    Add files via upload

---
+++ addingNUmbers in an array.js
@@ -0,0 +1,17 @@
+let numbers= [10,20,20,20]
+let addingDigits = 0
+for(x = 0 ; x < numbers.length; x++){
+    addingDigits += numbers[x]
+}
+console.log(addingDigits)
+// USING forEach function no need to add numbers[digits here]
+function adding(...numbers){
+    var sum = 0
+    numbers.forEach(digits=>{
+// USING forEach function no need to add numbers[digits here]
+
+        sum += digits
+
+    })
+    console.log(sum)
+}
